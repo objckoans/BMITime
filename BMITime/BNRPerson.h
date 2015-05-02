2eeb679ac6c9dc23d5e887ebdb4b07d371a60fad
Calvin Cheng <linchuan.cheng@gmail.com>
2015-05-02 23:38:48 +0800

    Simple Objective-C class.

---
+++ BMITime/BNRPerson.h
@@ -0,0 +1,23 @@
+//
+//  BNRPerson.h
+//  BMITime
+//
+//  Created by Calvin Cheng on 2/5/15.
+//  Copyright (c) 2015 Hello HQ Pte. Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRPerson : NSObject
+{
+    float _heightInMeters;
+    int _weightInKilos;
+}
+
+- (float)heightInMeters;
+- (void)setHeightInMeters:(float)h;
+
+- (int)weightInKilos;
+- (void)setWeightInKilos:(int)w;
+
+@end
---
+++ BMITime/BNRPerson.m
@@ -0,0 +1,29 @@
+//
+//  BNRPerson.m
+//  BMITime
+//
+//  Created by Calvin Cheng on 2/5/15.
+//  Copyright (c) 2015 Hello HQ Pte. Ltd. All rights reserved.
+//
+
+#import "BNRPerson.h"
+
+@implementation BNRPerson
+
+- (float)heightInMeters {
+    return _heightInMeters;
+}
+
+- (void)setHeightInMeters:(float)h {
+    _heightInMeters = h;
+}
+
+- (int)weightInKilos {
+    return _weightInKilos;
+}
+
+- (void)setWeightInKilos:(int)w {
+    _weightInKilos = w;
+}
+
+@end
